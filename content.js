3e1adff499b84747da985337c626827abde47100
aquamark-io <heyaquamark@gmail.com>
2025-05-03 22:11:56 -0700

    Update content.js

--- content.js
+++ content.js
@@ -24,13 +24,13 @@ function waitForGmailAttachments() {
       iconBtn.src = "https://www.aquamark.io/logo.png";
       iconBtn.title = "Watermark this file";
       iconBtn.className = "aquamark-icon-btn";
-      iconBtn.style.width = "28px";
-      iconBtn.style.height = "28px";
-      iconBtn.style.marginLeft = "6px";
-      iconBtn.style.cursor = "pointer";
-      iconBtn.style.verticalAlign = "middle";
-      iconBtn.style.borderRadius = "4px";
-      iconBtn.style.boxShadow = "0 0 2px rgba(0,0,0,0.2)";
+    iconBtn.style.width = "27px";
+iconBtn.style.height = "27px";
+iconBtn.style.margin = "0 4px";
+iconBtn.style.cursor = "pointer";
+iconBtn.style.verticalAlign = "top";
+iconBtn.style.borderRadius = "4px";
+iconBtn.style.boxShadow = "0 0 2px rgba(0,0,0,0.2)";
 
       iconBtn.addEventListener("click", async () => {
         const url = link.getAttribute("href");
