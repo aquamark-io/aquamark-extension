6d7bf972803257ca5091c59ee60f6da0af95ec1f
aquamark-io <heyaquamark@gmail.com>
2025-05-04 09:54:20 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
-// background.js
 chrome.tabs.onUpdated.addListener((tabId, info, tab) => {
-  if (info.status === "complete" && tab.url.includes("mail.google.com")) {
+  if (info.status === "complete" && tab.url && tab.url.includes("mail.google.com")) {
     chrome.scripting.executeScript({
       target: { tabId },
       files: ["content.js"]
